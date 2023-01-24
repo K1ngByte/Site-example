6eaec083496e651f2d677b251f4caffb90a07416
King <88986646+K1ngByte@users.noreply.github.com>
2023-01-24 01:28:38 -0300

    Add files via upload

---
+++ Responsive page/js/main.js
@@ -0,0 +1,10 @@
+function abrirmenu(){
+    let BarraMenuAberto = document.getElementById('Barra-menu')
+
+    if (BarraMenuAberto.style.width == "0px"){
+        BarraMenuAberto.style.width = "300px";
+    }else{
+        BarraMenuAberto.style.width = "0px";
+
+    }
+}
